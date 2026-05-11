353e2adff7d5da5f79ba9383aca9f58706edd4d3
Arvapalli Krishna Chaitanya <arvapallikrishnachaitanya@gmail.com>
2026-05-11 13:32:55 +0530

    Updated Arduino code

--- Arduino_Code/smart_energy_system.ino
+++ Arduino_Code/smart_energy_system.ino
@@ -1,10 +1,57 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int ldrPin = A0;
+int relayPin = 7;
+int solarLED = 8;
+int dieselLED = 9;
+
 void setup()
 {
+  pinMode(relayPin, OUTPUT);
+  pinMode(solarLED, OUTPUT);
+  pinMode(dieselLED, OUTPUT);
+
+  lcd.init();
+  lcd.backlight();
+
   Serial.begin(9600);
 }
 
 void loop()
 {
-  Serial.println("Solar System Active");
+  int lightValue = analogRead(ldrPin);
+
+  if(lightValue > 500)
+  {
+    digitalWrite(relayPin, HIGH);
+
+    digitalWrite(solarLED, HIGH);
+    digitalWrite(dieselLED, LOW);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("SOLAR MODE");
+
+    lcd.setCursor(0,1);
+    lcd.print("Diesel Saved");
+  }
+  else
+  {
+    digitalWrite(relayPin, LOW);
+
+    digitalWrite(solarLED, LOW);
+    digitalWrite(dieselLED, HIGH);
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("BACKUP MODE");
+
+    lcd.setCursor(0,1);
+    lcd.print("Using Diesel");
+  }
+
   delay(1000);
 }
